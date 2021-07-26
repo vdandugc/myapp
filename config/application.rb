ef69fc54faf1b199e480e129f56bb741d034ac48
vdandugc <vdandu@gocardless.com>
2021-07-26 22:23:10 +0100

    case 3

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
     # config.autoloader = :classic
 
     # config.eager_load_paths << "#{config.root}/app"
